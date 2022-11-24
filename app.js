4e6c3b27c5d5714df5047082668b135031340963
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2022-11-24 10:07:21 +0000

    notes added to code

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ import cors from "cors";
 
 // import in our resourcesRouter
 import router from "./routes/resources.js";
-
+//order of middleware is IMPORTANT!
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+//CORS should be before router
 app.use(cors("*"))
 app.use("/api/resources", router);
 
-
+//shows server listening to correct PORT
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
 });
--- db/index.js
+++ db/index.js
@@ -1,18 +1,22 @@
 // Open up connection pool using pg
 
 import pg from "pg";
+//pulling down enviromental vaiable from process.env
 const databaseUrl = process.env.POSTGRES_CONNECTION_URL;
 if (undefined === databaseUrl) {
+  //Throws error when database URL incorrect 
   throw new Error(
     "This project requires a database url. Did you forget to create a .env file? Please ensure a .env file exists and that it contains a DATABASE_URL variable."
   );
 }
+//opens new connection pool without having to reverify security 
 export const pool = new pg.Pool({
   connectionString: databaseUrl,
 });
 
 
 // And set way of making SQL queries from the app
+//ES6 way of exporting queries
 
 function query (text, params) {
   console.log("Text:", text, "params:", params)
