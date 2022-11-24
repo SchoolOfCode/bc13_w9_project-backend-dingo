fe4710ed2cc9de397089160587ed90aff2590317
Bella Bekanova <108465697+BellaBekanova@users.noreply.github.com>
2022-11-24 18:30:05 +0400

    completed testing

--- routes/resources.test.js
+++ routes/resources.test.js
@@ -4,6 +4,7 @@ import app from "../app";
 import request from "supertest";
 // import jest
 import { test, expect } from "@jest/globals";
+import {pool} from '../db'
 
 test("test to see if get route works", async () => {
 	const response = await request(app).get("/api/resources");
@@ -25,4 +26,37 @@ test("test to see if get route works", async () => {
 			title: expect.any(String),
 		});
 	}
+});
+
+
+test('test to see that the information is posted when typed', async ()=> {
+	const author = 'Coach';
+	const category = 'UX/UI';
+	const difficulty= 1;
+	const estimated_time= '< 30 mins';
+	const format = 'Video';
+	const link = 'https://www.youtube.com/watch?v=-yrnWnN0g9o';
+	const title= 'React With Firebase';
+    //const created = {task: task, completionDate: completionDate}
+    const response = await  request(app)
+        .post("/api/resources")
+        .send({ author, category, difficulty, estimated_time, format, link, title });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+        success: true,
+        payload: {
+             author: 'Coach',
+			 category :'UX/UI',
+	 		 difficulty: 1,
+	 		 estimated_time:'< 30 mins',
+	 		 format: 'Video',
+			 id: expect.any(Number),
+	 		 link:'https://www.youtube.com/watch?v=-yrnWnN0g9o',
+			 title:'React With Firebase',
+			
+        },
+    });
+})
+afterAll(async () => {
+    await pool.end();
 });
